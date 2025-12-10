c74182496510a0517aea1f189eaddce8e79f1074
vaibhav101agrawal-spec <vaibhav101agrawal@gmail.com>
2025-12-10 15:47:00 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',          // Needed for GitHub Pages
-  basePath: '/re-energen',   // must match your repo name
+  output: 'export',
+  basePath: '/re-energen',
   assetPrefix: '/re-energen/',
 };
 
